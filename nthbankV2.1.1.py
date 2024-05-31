d429d22a4c109787f015810d9da2a050199bf300
Nathan Soares <nathanbernardino09@gmail.com>
2024-05-31 14:21:24 -0300

    Nova versão V2.1.1

--- nthbankV2.1.1.py
+++ nthbankV2.1.1.py
@@ -84,6 +84,50 @@ def exibir_extrato(saldo, /, *, extrato):
     print(f"Saldo atual da conta: {saldo:.2f}")
     print("=================================")
 
+# Função criar usuário
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente número): ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        return
+
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("=== Usuário criado com sucesso! ===")
+
+# Função filtrar usuário
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+# Função criar conta
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+
+# Função listar contas
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta['agencia']}
+            C/C:\t\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
+        print("=" * 100)
+        print(textwrap.dedent(linha))
+
 # Função principal
 def main():
     LIMITE_SAQUES = 3
@@ -111,13 +155,18 @@ def main():
         elif opcao == "3":
             exibir_extrato(saldo, extrato=extrato)
         elif opcao == "4":
-            print("ESTA OPÇÃO NÃO ESTÁ DISPONIVEL NO MOMENTO")
+            criar_usuario(usuarios)
             sleep(2)
         elif opcao == "5":
-            print("ESTA OPÇÃO NÃO ESTÁ DISPONIVEL NO MOMENTO")
+            numero_conta = len(contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+            if conta:
+                contas.append(conta)
             sleep(2)
+
         elif opcao == "6":
-            print("ESTA OPÇÃO NÃO ESTÁ DISPONIVEL NO MOMENTO")
+            listar_contas(contas)
             sleep(2)
         elif opcao == "7":
             print(f"Obrigado por utilizar nosso sistema!")
